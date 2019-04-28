e82c2a0352b28ce29564956410543fe4034bdeaf
hughtan <hughtan@foxmail.com>
2019-04-28 09:33:41 +0800

    config

---
+++ readcomposerlockfile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hught
+ * Date: 2019/4/28
+ * Time: 8:57
+ */
+$file_path = 'D:\project\ochughtest\composer.lock';
+$str = file_get_contents($file_path);
+$arr = json_decode($str,true);
+$aa = array_merge($arr['packages'],$arr['packages-dev']);
+$newArr = [];
+foreach($aa as $k=>$v) {
+    $newArr['"'.$v['name'].'"'] = $v['version'];
+}
+echo json_encode($newArr);
